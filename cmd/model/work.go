4fbd3312f9f7ff748f3c5603efb5ef5d8be38c1a
hyeonwoody <hyeonwoody@gmail.com>
2025-02-22 04:44:19 +0900

    🛢️DB : Create model for `Work`

--- cmd/model/work.go
+++ cmd/model/work.go
@@ -11,5 +11,43 @@ type Work struct {
 	Destination string
 	Algorithm   craveModel.Algorithm
 	Step        craveModel.Step
-	Filter      craveModel.Filter
+	Filter      []craveModel.Filter
+}
+
+func WorkFrom(page, org, dest, algo, step, filter string) *Work {
+	return &Work{
+		Page:        craveModel.PageFromString(page),
+		Origin:      org,
+		Destination: dest,
+		Algorithm:   craveModel.AlgorithmFromString(algo),
+		Step:        craveModel.StepFromString(step),
+		Filter:      craveModel.FilterFromString(filter),
+	}
+}
+
+type WorkCache struct {
+	Page      craveModel.Page
+	Algorithm craveModel.Algorithm
+	Step      craveModel.Step
+	Filter    []craveModel.Filter
+}
+
+func (work *Work) ToCache() *WorkCache {
+	return &WorkCache{
+		Page:      work.Page,
+		Algorithm: work.Algorithm,
+		Step:      work.Step,
+		Filter:    append([]craveModel.Filter(nil), work.Filter...),
+	}
+}
+
+func (wc *WorkCache) Copy() *WorkCache {
+	copiedValue := &WorkCache{
+		Page:      wc.Page,
+		Algorithm: wc.Algorithm,
+		Step:      wc.Step,
+		Filter:    make([]craveModel.Filter, len(wc.Filter)),
+	}
+	copy(copiedValue.Filter, wc.Filter)
+	return copiedValue
 }
