53eb1687a110f67770fec5b116f0f040c9f68fab
hyeonwoody <hyeonwoody@gmail.com>
2025-02-22 04:53:25 +0900

    ✨FEAT : Implement library for hub

--- cmd/lib/hub.go
+++ cmd/lib/hub.go
@@ -1,68 +1,32 @@
 package lib
 
 import (
-	"context"
-	"crave/hub/cmd/configuration"
+	"crave/shared/configuration"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
-func Start(router *gin.Engine) error {
-	container := configuration.NewContainer(router)
+func Start(cntr configuration.IContainer) error {
 
 	errChan := make(chan error, 2)
 	go func() {
-		if err := startApiServer(container); err != nil {
+		if err := startApiServer(cntr); err != nil {
 			errChan <- fmt.Errorf("API Server error: %w", err)
 		}
 	}()
 	go func() {
-		if err := startGrpcServer(container); err != nil {
+		if err := startGrpcServer(cntr); err != nil {
 			errChan <- fmt.Errorf("gRPC Server error: %w", err)
 		}
 	}()
 
 	return <-errChan
 }
-func startApiServer(container *configuration.Container) error {
-	hubGroup := container.Router.Group("/hub")
-	{
-		hubGroup.POST("/work", container.HubHandler.Create)
-	}
-
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", container.Variable.HubApiPort),
-		Handler: container.Router,
-	}
-
-	fmt.Println("✅ API Server started on port", container.Variable.HubApiPort)
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("Server failed : %v", err)
-	}
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
-	<-quit
-
-	fmt.Println("\n⚠️  Shutting down server...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %v", err)
-	}
-
-	return nil
+func startApiServer(ctnr configuration.IContainer) error {
+	return ctnr.DefineRoute()
 }
 
-func startGrpcServer(container *configuration.Container) error {
+func startGrpcServer(ctnr configuration.IContainer) error {
+	return ctnr.DefineGrpc()
 	// lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", container.Variable.HubApiIp, container.Variable.HubApiPort))
 	// if err != nil {
 	// 	return fmt.Errorf("failed to listen : %d, %w", container.Variable.HubApiPort, err)
@@ -71,5 +35,4 @@ func startGrpcServer(container *configuration.Container) error {
 	// if servErr := s.Serve(lis); servErr != nil {
 	// 	return fmt.Errorf("failed to create server: %w", err)
 	// }
-	return nil
 }
