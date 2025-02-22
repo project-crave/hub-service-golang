6bd11af832521dc33e549be1645d3e6140803b40
hyeonwoody <hyeonwoody@gmail.com>
2025-02-22 09:02:30 +0900

    ✨FEAT : Implement to create `Work` with submodule

---
+++ cmd/api/presentation/handler/handler.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	hub "crave/hub/cmd/api/presentation/controller"
+	"crave/hub/cmd/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	ctrl hub.IController
+}
+
+func NewHandler(ctrl hub.IController) *Handler {
+
+	return &Handler{ctrl: ctrl}
+}
+
+func (h *Handler) CreateWork(c *gin.Context) {
+	input := model.Work{}
+	if err := c.ShouldBindQuery(&input); err != nil {
+		c.Status(http.StatusBadRequest)
+	}
+	h.ctrl.SaveWork(&input)
+	c.Status(http.StatusOK)
+}
