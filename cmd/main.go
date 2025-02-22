3b7889cedd1c46425fb31ae697ee86d1ce27251b
hyeonwoody <hyeonwoody@gmail.com>
2025-02-22 09:04:21 +0900

    🎨REFACTOR : Modify main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"crave/hub/cmd/configuration"
 	"crave/hub/cmd/lib"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	//router.Use()
-
 	startLib()
 }
 
 func startLib() {
-	lib.Start(nil)
+	router := gin.Default()
+	ctnr := configuration.NewHubWorkContainer(router)
+	lib.Start(ctnr)
 }
