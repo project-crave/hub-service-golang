2b2b161c2b1a52de88d75adb1f6b0a07b5613c70
hyeonwoody <hyeonwoody@gmail.com>
2025-02-24 20:25:20 +0900

    🛢️DB : Unify `Target` data structure

--- cmd/model/target.go
+++ cmd/model/target.go
@@ -12,39 +12,3 @@ type Target struct {
 func (Target) TableName() string {
 	return "target"
 }
-
-// type TargetStack struct {
-// 	Id       uint   `gorm:"primaryKey;autoIncrement"`
-// 	Work_id  uint   `gorm:"primaryKey;uniqueIndex:idx_target_stack"`
-// 	Name     string `gorm:"uniqueIndex:idx_target_stack;type:varchar(20);not null"`
-// 	Previous uint   `gorm:"uniqueIndex:idx_target_stack;not null"`
-// 	Done     bool   `gorm:"default:false"`
-// }
-
-// func (TargetStack) TableName() string {
-// 	return "target_stack"
-// }
-
-// func (ts *TargetStack) BeforeCreate(tx *gorm.DB) error {
-// 	newId, err := beforeCreate(tx, ts.TableName(), ts.Work_id)
-// 	ts.Id = newId
-// 	return err
-// }
-
-// type TargetQueue struct {
-// 	Id       uint   `gorm:"primaryKey;autoIncrement"`
-// 	Work_id  uint   `gorm:"primaryKey;uniqueIndex:idx_target_queue"`
-// 	Name     string `gorm:"type:varchar(20);not null;uniqueIndex:idx_target_queue"`
-// 	Previous uint   `gorm:"uniqueIndex:idx_target_queue;not null"`
-// 	Done     bool   `gorm:"default:false"`
-// }
-
-// func (TargetQueue) TableName() string {
-// 	return "target_queue"
-// }
-
-// func (tq *TargetQueue) BeforeCreate(tx *gorm.DB) error {
-// 	newId, err := beforeCreate(tx, tq.TableName(), tq.Work_id)
-// 	tq.Id = newId
-// 	return err
-// }
